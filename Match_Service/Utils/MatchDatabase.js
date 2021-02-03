f72fa5ee6099606d804d7a05967e943b1ac88c7d
Jerry S Joseph <jerin.sebastian153@gmail.com>
2021-02-03 13:31:46 +0530

    match database URI fix

--- Match_Service/Utils/MatchDatabase.js
+++ Match_Service/Utils/MatchDatabase.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 let DB_URI="mongodb://localhost:27017/one_day_matches_db"
 if(process.env.MATCH_DB_CONNECTION_STRING)
     DB_URI=process.env.MATCH_DB_CONNECTION_STRING
-var matchdatabase=mongoose.createConnection(process.env.MATCH_DB_CONNECTION_STRING,{useNewUrlParser:true,useUnifiedTopology:true});
+var matchdatabase=mongoose.createConnection(DB_URI,{useNewUrlParser:true,useUnifiedTopology:true});
 const Init=function(callback){
     
-    mongoose.connect(process.env.MATCH_DB_CONNECTION_STRING,
+    mongoose.connect(DB_URI,
             {useNewUrlParser:true,useUnifiedTopology:true},
             (error)=>{
             callback(error)
